c4ec240c5e431ce09ad2024b472a71ed7d16fe0d
mousa nageh <200moussa200@gmail.com>
2024-02-13 20:45:20 +0200

    refactor create materialized view to be one function

--- user/management/commands/user_pg_materialized_views.py
+++ user/management/commands/user_pg_materialized_views.py
@@ -10,14 +10,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         with connection.cursor() as cursor:
             try:
-                cursor.execute(
-                    f"""
-                    BEGIN;
-                    {self._user_statistics_materialized_views_sql()}
-                    {self._create_index_for_m_view_sql()}
-                    COMMIT;
-                """
-                )
+                cursor.execute(self._create_index_and_materialized_view_sql())
                 self.stdout.write(
                     self.style.SUCCESS(
                         f"'{PG_USER_PER_WEEK_MATERIALIZED_VIEW_NAME}' "
@@ -28,42 +21,48 @@ class Command(BaseCommand):
                 self.stdout.write(self.style.ERROR(str(e)))
                 raise e
 
-    @staticmethod
-    def _user_statistics_materialized_views_sql():
-        return f"""
-            DROP MATERIALIZED VIEW IF EXISTS {PG_USER_PER_WEEK_MATERIALIZED_VIEW_NAME};
-            CREATE MATERIALIZED VIEW {PG_USER_PER_WEEK_MATERIALIZED_VIEW_NAME} AS (
-                SELECT
-                    ROW_NUMBER() OVER() AS id,
-                    week::DATE,
-                    SUM(country_week_count)::INTEGER AS user_number,
-                    jsonb_agg(jsonb_build_object(country_code, country_week_count)) AS counties
-                FROM (
-                    SELECT 
-                        country_code,
-                        DATE_TRUNC('WEEK', created_at) AS week,
-                        count(*) AS country_week_count
-                    FROM {USER_TABLE_NAME}
-                    GROUP BY country_code, week
-                ) AS country_week_table
-                GROUP BY week 
-                ORDER BY week
-            );
-        """
-
-    @staticmethod
-    def _create_index_for_m_view_sql():
+    @classmethod
+    def _create_index_and_materialized_view_sql(cls):
         return f"""
             DO $$
             BEGIN
+               IF NOT EXISTS (SELECT FROM pg_matviews WHERE  matviewname = '{PG_USER_PER_WEEK_MATERIALIZED_VIEW_NAME}')
+                THEN
+                    EXECUTE '{cls._user_statistics_materialized_views_sql()}';
+                END IF;
                 IF NOT EXISTS (
                     SELECT 1 FROM pg_class c
                     JOIN   pg_namespace n ON n.oid = c.relnamespace
                     WHERE  c.relname = 'user_per_week_m_view_id_idx'
                 ) THEN
-                    EXECUTE 'CREATE UNIQUE INDEX user_per_week_m_view_id_idx ON 
-                    {PG_USER_PER_WEEK_MATERIALIZED_VIEW_NAME}(id)';
+                    EXECUTE '{cls._create_index_from_materialized_view_sql()}';
                 END IF;
             END
             $$;
         """
+
+    @staticmethod
+    def _user_statistics_materialized_views_sql():
+        return f"""
+                CREATE MATERIALIZED VIEW {PG_USER_PER_WEEK_MATERIALIZED_VIEW_NAME} AS (
+                    SELECT
+                        ROW_NUMBER() OVER() AS id,
+                        week::DATE,
+                        SUM(country_week_count)::INTEGER AS user_number,
+                        jsonb_agg(jsonb_build_object(country_code, country_week_count)) AS counties
+                    FROM (
+                        SELECT 
+                            country_code,
+                            DATE_TRUNC(''WEEK'', created_at) AS week,
+                            count(*) AS country_week_count
+                        FROM {USER_TABLE_NAME}
+                        GROUP BY country_code, week
+                    ) AS country_week_table
+                    GROUP BY week 
+                    ORDER BY week
+                );
+            """
+
+    @staticmethod
+    def _create_index_from_materialized_view_sql():
+        return f"CREATE UNIQUE INDEX user_per_week_m_view_id_idx ON {PG_USER_PER_WEEK_MATERIALIZED_VIEW_NAME}(id);"
