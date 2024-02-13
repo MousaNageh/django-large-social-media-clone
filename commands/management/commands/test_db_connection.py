ab0dc7a184d5c9d1ee457a59b8a7a169c2f2f74b
mousa nageh <moussa.nageh@umbrasys.com>
2024-02-13 17:40:54 +0200

    add connection database test

--- commands/management/commands/test_db_connection.py
+++ commands/management/commands/test_db_connection.py
@@ -1,7 +1,7 @@
-
 from django.core.management.base import BaseCommand
 from django.db import connections
 import time
+
 
 class Command(BaseCommand):
     help = "wait until db connection is established"
@@ -10,13 +10,12 @@ class Command(BaseCommand):
         i = 1
         while i < 5:
             try:
-                connections['default'].cursor()
+                connections["default"].cursor()
                 break
             except Exception as e:
                 print("database not ready to connect")
+                print(e)
                 time.sleep(5)
                 i += 1
 
-        self.stdout.write(
-            self.style.SUCCESS(f'data base ready to connect')
-        )
+        self.stdout.write(self.style.SUCCESS("data base ready to connect"))
