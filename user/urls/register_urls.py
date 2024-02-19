af93185cf556c7de24a6840189c0b9c62435572f
mousa nageh <moussa.nageh@umbrasys.com>
2024-02-19 17:00:22 +0200

    finish login with google

--- user/urls/register_urls.py
+++ user/urls/register_urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from user.views import RegisterBySystem
+from user.views.user_register_by_google import RegisterByGoogleAPIView
 
 urlpatterns = [
-    path("system", RegisterBySystem.as_view(), name="register-by-system-api")
+    path("system", RegisterBySystem.as_view(), name="register-by-system-api"),
+    path("google", RegisterByGoogleAPIView.as_view(), name="register-by-google-api"),
 ]
